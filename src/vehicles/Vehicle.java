22befdd890f5f445e0149d9b9eff4460f964f421
Hus <110332667+HusseinBader2002@users.noreply.github.com>
2025-05-02 23:03:07 -0500

    Update Vehicle.java

--- src/vehicles/Vehicle.java
+++ src/vehicles/Vehicle.java
@@ -1,19 +1,15 @@
-package vehicles;
-
+// Vehicle.java - Abstract class to enforce common behavior
 public abstract class Vehicle {
-    private String make;
-    private String model;
-    private int year;
+    private String make; // Made private to enforce encapsulation
 
-    public Vehicle(String make, String model, int year) {
+    public Vehicle(String make) {
         this.make = make;
-        this.model = model;
-        this.year = year;
     }
 
-    public int getYear() {
-        return year;
+    public String getMake() { // Getter for encapsulated field
+        return make;
     }
 
+    // Abstract method to be implemented by all vehicles
     public abstract void displayInfo();
 }
